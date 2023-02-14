23581231a4975f0e485986a67dc1ad4a00abb5bf
Reece Taylor <reece.taylor@live.com>
2023-02-14 11:25:18 +0000

    added update function

--- test/playground/Hello.test.ts
+++ test/playground/Hello.test.ts
@@ -1,9 +1,12 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
-import { handler } from '../../services/SpacesTable/Read'
+import { handler } from '../../services/SpacesTable/Update'
 
 const event: APIGatewayProxyEvent = {
     queryStringParameters: {
-        location: 'London'
+        spaceId: 'dd5bd6b9-5b13-41c7-a4c8-9d1355ababab'
+    },
+    body: {
+        location: 'New Location'
     }
 } as any;
 
